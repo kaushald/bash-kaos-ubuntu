6f56157ef62757934e3e47877bf068bda7f12c95
Kaushal Dalvi <kaushaldalvi@Kaushals-MacBook-Pro.local>
2016-11-24 10:22:21 -0500

    Remove extra PS1

--- bashrc.sh
+++ bashrc.sh
@@ -13,7 +13,6 @@ export GIT_PS1_SHOWDIRTYSTATE=1
 # '\u' adds the name of the current user to the prompt
 # '\$(__git_ps1)' adds git-related stuff
 # '\W' adds the name of the current directory
-export PS1="$purple\u$green\$(__git_ps1)$blue \W $ $reset"
 
 export JAVA_HOME=$(/usr/libexec/java_home)
 
